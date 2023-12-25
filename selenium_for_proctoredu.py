293f08c9318cdd827b8193a05bf8ec349026cb07
SGVp15 <phosphorusp15@mail.ru>
2023-12-25 14:00:12 +0300

    registration

--- selenium_for_proctoredu.py
+++ selenium_for_proctoredu.py
@@ -77,7 +77,6 @@ class Proctor:
             pass
 
         xpath = '/html/body/div[3]/div[2]/div[2]/div[1]/div/div[7]/div/button'
-        # xpath = ''
         while True:
             try:
                 self.driver.get('https://itexpert.proctoring.online/#!/users')
