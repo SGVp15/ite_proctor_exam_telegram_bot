6d5c07119c8bd3059d021fb2107f2aef9d8ed893
SGVp15 <phosphorusp15@mail.ru>
2023-12-25 16:11:35 +0300

    registration

--- Config/config.py
+++ Config/config.py
@@ -136,8 +136,8 @@ EMAIL_LOGIN = config.get('EMAIL_LOGIN')
 SMTP_SERVER = 'smtp.yandex.ru'
 SMTP_PORT = 465
 
-EMAIL_BСС = 'v.gromakov@itexpert.ru', 'g.savushkin@itexpert.ru', 'o.kuprienko@itexpert.ru'
-EMAIL_BСС_course = 'g.savushkin@itexpert.ru', 'a.rybalkin@itexpert.ru', 'kab@itexpert.ru'
+EMAIL_BСС = ['v.gromakov@itexpert.ru', 'g.savushkin@itexpert.ru', 'o.kuprienko@itexpert.ru']
+EMAIL_BСС_course = ['g.savushkin@itexpert.ru', 'a.rybalkin@itexpert.ru', 'kab@itexpert.ru']
 
 email_login_password = {}
 template_folder = './Email/template_email/'
