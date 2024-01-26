366e942285fa88d618058e283c03ff6d0b7110fa
SGVp15 <phosphorusp15@mail.ru>
2024-01-26 12:03:14 +0300

    aiogram3.3

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import asyncio
 
 from Telegram.main import start_bot
 
+
+async def main():
+    tasks = [
+        start_bot(),
+    ]
+    await asyncio.gather(*tasks)
+
+
 if __name__ == '__main__':
     print('Exam_Registration_bot start')
-    asyncio.run(start_bot())
+    asyncio.run(main())
