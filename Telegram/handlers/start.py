25c895066f493c869cc0efc1a3646246a80a99f8
SGVp15 <phosphorusp15@mail.ru>
2024-01-26 12:02:08 +0300

    aiogram3.3

--- Telegram/handlers/start.py
+++ Telegram/handlers/start.py
@@ -8,7 +8,7 @@ from Telegram.keybords import inline
 from Telegram.main import dp
 
 
-@dp.message(Command('start', 'help') & F.from_user.id.in_(*ADMIN_ID, ))
+@dp.message(F.command.in_({'start', 'help'}) & F.from_user.id.in_({*ADMIN_ID}))
 async def send_welcome_admin(message: types.Message, state: FSMContext):
     text = f'Здравствуйте , {message.from_user.first_name}! \n'
     text += f'Этот бот работает с ProctorEDU.'
@@ -16,14 +16,14 @@ async def send_welcome_admin(message: types.Message, state: FSMContext):
     await message.answer(text=text, reply_markup=inline.inline_kb_main)
 
 
-@dp.message(Command('start', 'help') & F.from_user.id.in_({*USERS_ID}) & F.state.in_({'*', None}))
+@dp.message(F.command.in_({'start', 'help'}) & F.from_user.id.in_({*USERS_ID}))
 async def send_welcome_new_user(message: types.Message):
     text = f'Здравствуйте, {message.from_user.first_name}.'
     text += f'\n ❓/id - узнать ваш id'
     await message.answer(text=text, reply_markup=inline.inline_kb_main)
 
 
-@dp.message(Command('start', 'help') & F.state.in_({'*', None}))
+@dp.message(Command('start', 'help'))
 async def send_welcome(message: types.Message):
     text = f'Здравствуйте, {message.from_user.first_name}.'
     text += f'\n ❓/id - узнать ваш id'
--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from Telegram.main import start_bot
 
 if __name__ == '__main__':
     print('Exam_Registration_bot start')
-    # loop.create_task(registration())
     asyncio.run(start_bot())
