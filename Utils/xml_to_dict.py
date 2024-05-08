bdde5a16c0e2a47e9a28b891a5f8257687155279
SGVp15 <phosphorusp15@mail.ru>
2024-05-08 22:15:18 +0300

    show_registration

--- Utils/xml_to_dict.py
+++ Utils/xml_to_dict.py
@@ -3,16 +3,12 @@ import xmltodict
 
 def get_ispring_users(s: str) -> list[dict]:
     my_dict = xmltodict.parse(s)
-
-    users_from_ispring: list = my_dict.get('response').get('userProfile')
-
-    for user in users_from_ispring:
+    users_ispring: list = my_dict.get('response').get('userProfile')
+    for user in users_ispring:
         fields: list = user.get('fields').get('field')
-
         for field in fields:
             user[field.get('name')] = field.get('value')
-
-    return users_from_ispring
+    return users_ispring
 
 
 def get_ispring_enrollments(s: str) -> list[dict]:
