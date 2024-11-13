de47bfdeebff8f407f60d5c95c45d118dafe6ff4
SGVp15 <phosphorusp15@mail.ru>
2024-11-13 13:20:27 +0300

    Contact replace

--- test_parser.py
+++ test_parser.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
 from Contact import Contact
-from parser import get_contact_from_data
+from parser import get_contact_from_excel
 
 
 class Test(TestCase):
     def test_get_contact_from_excel(self):
-        users: [Contact] = get_contact_from_data()
+        users: [Contact] = get_contact_from_excel()
         # self.fail()
