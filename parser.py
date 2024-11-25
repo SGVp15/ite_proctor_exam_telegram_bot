fc8173be296dabf6f8593b11532b9c83983759eb
SGVp15 <phosphorusp15@mail.ru>
2024-11-25 15:08:08 +0300

    refactoring

--- parser.py
+++ parser.py
@@ -52,33 +52,24 @@ def get_contact_from_array(data_list) -> list[Contact]:
         # Minute_column: str = 'J'	9
         # Proctor_column: str = 'K'	10
 
-        if data[0] is None:
+        if not data[0]:
             continue
         user.last_name_rus = data[0]
-        if user.last_name_rus in (None, ''):
-            continue
         user.first_name_rus = data[1]
         user.last_name_eng = data[2]
         user.first_name_eng = data[3]
         user.email = data[4]
         user.password = data[5]
         user.exam = data[6]
-        user.date_from_file = data[7].lower()
+        user.date_from_file = data[7].lower().strip()
         hour = int(data[8])
         minute = int(data[9])
-
         user.proctor = data[10]
-        t = user.date_from_file
-        t = re.sub(r'[^\d.]', '', t)
-        t = t.split('.')
-        t = list(map(int, t))
-        # 2023-02-02T13:29:31Z
 
-        # contact.dateExam = datetime.datetime(2023, 5, 13, hour, minute)
-        year = t[2]
-        month = t[1]
-        day = t[0]
-        user.date_exam = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+        date_string = user.date_from_file
+        date_string = re.sub(r'[^\d.]', '', date_string)
+        # 2023-02-02T13:29:31Z
+        user.date_exam = datetime.datetime.strptime(f"{date_string}-{hour}:{minute}", "%d.%m.%Y-%H:%M")
 
         if user.normalize():
             users.append(user)
