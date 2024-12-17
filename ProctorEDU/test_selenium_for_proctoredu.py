a3c06349e2e3cfe381e356d62045c458eccdd41a
SGVp15 <phosphorusp15@mail.ru>
2024-12-18 00:06:04 +0300

    refactoring

--- ProctorEDU/test_selenium_for_proctoredu.py
+++ ProctorEDU/test_selenium_for_proctoredu.py
@@ -10,5 +10,8 @@ class TestProctorEduSelenium(TestCase):
         driver = ProctorEduSelenium()
         asyncio.run(driver.authorization())
         time.sleep(1)
+        asyncio.run(driver.create_users_and_session())
         if driver.driver.current_url != 'https://itexpert.proctoring.online/#!/rooms':
             self.fail()
+
+
