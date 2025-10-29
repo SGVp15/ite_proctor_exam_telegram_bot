067e9c17499f2310070887d53148a58171e028e0
SGVp15 <phosphorusp15@mail.ru>
2025-10-29 17:08:57 +0300

    exam@itexpert.ru

--- Email/config.py
+++ Email/config.py
@@ -8,8 +8,8 @@ EMAIL_PASSWORD = config.get('EMAIL_PASSWORD')
 EMAIL_LOGIN = config.get('EMAIL_LOGIN')
 SMTP_SERVER = 'smtp.yandex.ru'
 SMTP_PORT = 465
-EMAIL_BCC = ['g.savushkin@itexpert.ru', 'o.kuprienko@itexpert.ru', 'p.moiseenko@itexpert.ru']
-EMAIL_BCC_course = ['g.savushkin@itexpert.ru', 'a.rybalkin@itexpert.ru', 'kab@itexpert.ru']
+EMAIL_BCC = ['exam@itexpert.ru',]
+EMAIL_BCC_course = ['exam@itexpert.ru',]
 email_login_password = {}
 
 TEMPLATE_FOLDER: str = os.path.join(os.getcwd(), 'Email', 'template_email')
