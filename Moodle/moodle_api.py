8b31fe0e1d944f42fb2143da5584ee97de76ab80
SGVp15 <phosphorusp15@gmail.com>
2025-11-19 17:26:05 +0300

    moodle exam

--- Moodle/moodle_api.py
+++ Moodle/moodle_api.py
@@ -66,7 +66,7 @@ class MOODLE_API:
         url_with_params = self.__get_url_with_params(function_name)
         new_user_data = {
             'username': f'{user.username}', 'password': f'{user.password}',
-            'firstname': f'{user.first_name_rus}', 'lastname': f'{user.last_name_eng}',
+            'firstname': f'{user.first_name_rus}', 'lastname': f'{user.last_name_rus}',
             'email': f'{user.email}',
         }
         post_data_dict = self.__format_post_array(new_user_data, array_name='users')
