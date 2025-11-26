0d0aad924900a4ad6ba4c296251f444e650fc51b
SGVp15 <phosphorusp15@gmail.com>
2025-11-26 12:08:52 +0300

    check_log_and_send_email()

--- Utils/check_log_send_email.py
+++ Utils/check_log_send_email.py
@@ -1,12 +1,21 @@
 from asyncio import sleep
 
+from Contact import Contact
 from Email import EmailSending
 from Email.config import EMAIL_LOGIN, SMTP_SERVER, SMTP_PORT, EMAIL_PASSWORD, EMAIL_BCC
+from config import LOG_FILE
 
 
 def exam_date():
     data = ''
+    with open(LOG_FILE, 'r', encoding='utf-8') as f:
+        f.read()
     return data
+
+
+def parser_str_contact(s: str):
+    c = Contact()
+    return c
 
 
 async def check_log_and_send_email():
