df0040464312c07c5c1e77eda86da70732b4dfb6
SGVp15 <phosphorusp15@gmail.com>
2025-11-28 16:09:17 +0300

    class ITEXPERT_API

--- Telegram/handlers/error.py
+++ Telegram/handlers/error.py
@@ -8,7 +8,7 @@ from Telegram.main import dp
 async def echo(message: types.Message):
     await message.reply('Не понимаю, что это значит.'
                         'Воспользуйтесь командой /help')
-    with open(LOG_FILE, encoding='utf-8', mode='a') as f:
-        for k, v in message:
-            f.write(f'{k} {v}', )
-        f.write('\n')
+    # with open(LOG_FILE, encoding='utf-8', mode='a') as f:
+    #     for k, v in message:
+    #         f.write(f'{k} {v}', )
+    #     f.write('\n')
