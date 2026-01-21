79d7cd56cd0e41d9a759d922f812474049e8a4e2
SGVp15 <phosphorusp15@gmail.com>
2026-01-21 18:20:10 +0300

    CallBackData.SENT_REPORT_AND_CERT_LK

--- Telegram/handlers/user.py
+++ Telegram/handlers/user.py
@@ -45,7 +45,9 @@ async def download_document_handle(message: message):
 @dp.callback_query(F.data.in_({CallBackData.SENT_REPORT_AND_CERT_LK}))
 async def btn_sent_report_and_cert_lk(callback_query: types.callback_query):
     await sent_report_and_cert_lk(date=datetime.datetime.now())
-    await message.answer('sent_report_and_cert_lk', reply_markup=inline_kb_main)
+    await bot.send_message(text='SENT_REPORT_AND_CERT_LK', chat_id=callback_query.from_user.id,
+                           message_id=callback_query.message.message_id,
+                           reply_markup=inline_kb_main)
 
 # @dp.callback_query(F.data.in_({CallBackData.EDIT_REGISTRATION}) & F.from_user.id.in_({*ADMIN_ID, *USERS_ID}))
 # async def show_registration(callback_query: CallbackQuery):
