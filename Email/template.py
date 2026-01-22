84e167168362dca1b21de72ebf2913a35b4d1846
SGVp15 <phosphorusp15@gmail.com>
2026-01-22 17:06:22 +0300

    email html

--- Email/template.py
+++ Email/template.py
@@ -1,2 +1,4 @@
 template_email_registration_exam_offline = 'email_registration_exam_offline.txt'
 template_email_registration_exam_online = 'email_registration_exam_online.txt'
+template_email_cert_exam = 'exam_result_passed.html'
+template_email_cert_exam = 'exam_result_failed.txt'
