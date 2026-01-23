c9e03ebce9ec5c3580de6d4ccbf96bc115bcb401
SGVp15 <phosphorusp15@gmail.com>
2026-01-23 12:39:45 +0300

    html templates

--- Email/test_email_sending.py
+++ Email/test_email_sending.py
@@ -1,26 +1,30 @@
+import datetime
 from unittest import TestCase
 
+from Contact import load_contacts_from_log_file
 # from Config.config import user_id_email
 # from Contact import parser
 
-from Email import EmailSending
+from Email import EmailSending, template_email_registration_exam_offline, template_email_registration_exam_online
+from Email.template import template_email_exam_result_passed, template_email_exam_result_failed
 
-
-# from My_jinja import MyJinja
+from My_jinja import MyJinja
 
 
 class TestEmailSending(TestCase):
     @staticmethod
     def test_send_email():
-        s = '''Курс: «Проверка отправки почты
-        OPS-online
-        Даты проведения курса:	99.99.2000 - 80.90.2003 5 занятий с 10:00 до 14:00 мск (25 ак.ч. с тренером +7 ак.ч. на самост.вып.ДЗ)
-        Тренер:	Сапегин Степан Борисович
-        Место проведения:	Webinar_1
-        Идентификатор конференции:	+
-        Код доступа:
-        Ссылка для регистрации:	https://events.webinar.ru/event/999146969/1581189808/edit
-        №	ФИО		Организация		Должность		e-mail
-        1	Григорьева Сабина 					asdasdqdq@stadasdep.rasdasdu	'''
-
-        EmailSending(to='g.savushkin@itexpert.ru', text='text', html='html').send_email()
+        contacts = load_contacts_from_log_file(filtered_date=datetime.datetime.now())
+        print(contacts)
+        for contact in contacts:
+            templates = [
+                # template_email_registration_exam_offline,
+                #          template_email_registration_exam_online,
+                         template_email_exam_result_passed,
+                         template_email_exam_result_failed,
+                         ]
+            for template in templates:
+                text = MyJinja(template_file=template).render_document(user=contact)
+                EmailSending(to='g.savushkin@itexpert.ru',
+                             # text='text',
+                             html=text).send_email()
