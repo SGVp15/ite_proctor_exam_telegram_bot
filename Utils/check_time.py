d8ad78353227b684f96076097507674d5c4573ff
SGVp15 <phosphorusp15@gmail.com>
2026-01-23 10:06:28 +0300

    email html

--- Utils/check_time.py
+++ Utils/check_time.py
@@ -1,9 +1,15 @@
 import datetime
 
 
-def check_time_interval(check_dt: datetime.datetime, start_dt: datetime.datetime,
-                        delta_dt: datetime.timedelta | None = None, end_dt: datetime.datetime | None = None) -> bool:
-    ''' Если введены delta_dt и end_dt - приоритет end_dt.'''
-    if not end_dt:
-        end_dt = start_dt + delta_dt
+def check_time_interval(check_dt: datetime.datetime,
+                        start_dt: datetime.datetime,
+                        delta_dt: datetime.timedelta | None = None,
+                        end_dt: datetime.datetime | None = None) -> bool:
+    ''' Если введены delta_dt и end_dt - приоритет end_dt. '''
+
+    if end_dt is None:
+        if delta_dt is not None:
+            end_dt = start_dt + delta_dt
+        else:
+            end_dt = start_dt
     return start_dt <= check_dt <= end_dt
