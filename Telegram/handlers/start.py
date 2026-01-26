cd66092205ccb6fd013e33d3b2ea355ebc69f2aa
SGVp15 <phosphorusp15@gmail.com>
2026-01-26 10:49:17 +0300

    VERSION

--- Telegram/handlers/start.py
+++ Telegram/handlers/start.py
@@ -3,9 +3,11 @@ from aiogram.fsm.context import FSMContext
 
 from aiogram.filters import Command
 
+from Telegram.Call_Back_Data import CallBackData
+from Telegram.keybords.inline import inline_kb_main
 from root_config import ADMIN_ID, USERS_ID, VERSION
 from Telegram.keybords import inline
-from Telegram.main import dp
+from Telegram.main import dp, bot
 
 
 @dp.message(F.command.in_({'start', 'help'}) & F.from_user.id.in_({*ADMIN_ID}))
@@ -32,6 +34,12 @@ async def send_welcome(message: types.Message):
     text += f'\n Version = {VERSION}'
 
     await message.answer(text=text, reply_markup=inline.inline_kb_main)
+
+
+@dp.callback_query(F.data.in_({CallBackData.SHOW_VERSION}))
+async def btn_sent_report_and_cert_lk(callback_query: types.callback_query):
+    await bot.send_message(text=f'{VERSION}', chat_id=callback_query.from_user.id,
+                           reply_markup=inline_kb_main)
 
 
 @dp.message(Command('id'))
