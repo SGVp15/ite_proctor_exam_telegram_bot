a0a97ea7a3d428558d29139c6a650a51c6be51a8
user <phosphorusp15@gmail.com>
2026-03-26 14:27:53 +0300

    main

--- Telegram/handlers/log.py
+++ Telegram/handlers/log.py
@@ -89,7 +89,12 @@ async def show_exam_now(callback_query: types.callback_query):
         rows = []
 
         for c in contacts:
-            rows.append(f'{c.date_exam.strftime("%H:%M")} {c.exam} {c.email} {c.last_name_rus} {c.first_name_rus}')
+            offline = ''
+            if c.proctor in (0, '0', False, None):
+                offline = 'offline'
+
+            rows.append(
+                f'{c.date_exam.strftime("%H:%M")} {c.exam} {c.email} {c.last_name_rus} {c.first_name_rus} {offline}')
 
         if rows:
             rows = [f'{i + 1}. {v}' for i, v in enumerate(sorted(rows))]
@@ -114,13 +119,11 @@ async def show_all_exams(callback_query: types.callback_query):
         c: Contact
         rows = []
         for c in contacts:
-            if c.proctor:
-                online = 'online'
-            else:
-                online = 'offline'
+            offline = ''
+            if c.proctor in (0, '0', False, None):
+                offline = 'offline'
             rows.append(
-                f'{c.date_exam.strftime("%Y.%m.%d %H:%M")} {c.exam} {c.email} {c.last_name_rus} {c.first_name_rus} {c.proctor
-                } {online}')
+                f'{c.date_exam.strftime("%Y.%m.%d %H:%M")} {c.exam} {c.email} {c.last_name_rus} {c.first_name_rus} {offline}')
 
         if rows:
             rows = [f'{i + 1}. {v}' for i, v in enumerate(sorted(rows))]
