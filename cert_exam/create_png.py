effcb44aaa0e9c8625f47937bdbf5d513a365efa
user <phosphorusp15@gmail.com>
2026-03-30 14:55:55 +0300

    cert ..

--- cert_exam/create_png.py
+++ cert_exam/create_png.py
@@ -14,17 +14,17 @@ def create_png(cert_contact: CertContact):
 
     image_draw = ImageDraw.Draw(img)
 
-    Geometria_70 = ImageFont.truetype(Path('./fonts/Geometria/geometria_light.otf'), 70)
+    Geometria_70 = ImageFont.truetype(Path('./data/fonts/Geometria/geometria_light.otf'), 70)
     image_draw.text((647, 1220),
                     f'№ {cert_contact.number:06}   {cert_contact.date_exam.strftime('%d.%m.%Y')}', font=Geometria_70,
                     fill=(16, 21, 84))
 
-    Rubik_115 = ImageFont.truetype(Path('./fonts/Rubik/Rubik-Regular.ttf'), 115, )
+    Rubik_115 = ImageFont.truetype(Path('./data/fonts/Rubik/Rubik-Regular.ttf'), 115, )
     image_draw.text((round(0.59 * img.width), 1550),
                     f'{cert_contact.name_rus}', font=Rubik_115, fill=(16, 21, 84),
                     anchor='mm')
 
-    Rubik_70 = ImageFont.truetype('./fonts/Rubik/Rubik-Regular.ttf', 70)
+    Rubik_70 = ImageFont.truetype('./data/fonts/Rubik/Rubik-Regular.ttf', 70)
     image_draw.text((round(0.59 * img.width), 1700),
                     f'{cert_contact.name_eng}', font=Rubik_70, fill=(16, 21, 84), anchor='mm')
 
---
+++ tes_cert_exam.py
@@ -0,0 +1,4 @@
+from cert_exam.main_cert_exam import sheduler_main_create_exam_cert
+
+if __name__ == '__main__':
+    sheduler_main_create_exam_cert()
